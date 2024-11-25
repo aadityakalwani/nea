0c6f381db653eeeb3c1d105af7fbd011ab59d26d
aadityakalwani <aadityakalwani@gmail.com>
2024-11-25 17:53:31 +0000

    using my own random method for extra points

--- Program.cs
+++ Program.cs
@@ -138,16 +138,6 @@ namespace bobFinal
             return Name;
         }
 
-        public int getMaxValue()
-        {
-            return MaxValue;
-        }
-
-        public void setValue(float value)
-        {
-            Value = value;
-        }
-
         public void ChangeQuantity(float amount)
         {
             Value = Math.Max(0, Math.Min(MaxValue, Value + amount));
@@ -175,19 +165,37 @@ namespace bobFinal
 
     public static class Market
     {
-        private static readonly Random Random = new Random();
+        private static readonly customRandom customRandom = new customRandom();
 
         public static void UpdateConversionRates(List<Resource> resources)
         {
             foreach (var resource in resources)
             {
                 // fluctuate the conversion rate by upto +/- 10%
-                var fluctuation = Random.Next(-10, 11);
+                var fluctuation = customRandom.Next(-10, 11);
                 resource.ConversionRate += resource.ConversionRate * fluctuation / 100;
 
                 // ensure the conversion rate is at least 1
                 if (resource.ConversionRate < 1) resource.ConversionRate = 1;
             }
+        }
+    }
+
+    public class customRandom
+    {
+        public customRandom() { }
+
+        // Method to generate the next random number in the range [min, max)
+        public int Next(int min, int max)
+        {
+            // use the current time's ticks and some operations
+            // 1 tick = 1 x 10^-7 seconds
+            long currentTicks = DateTime.Now.Ticks;
+
+            // increase variability and ensure the result is within the range
+            long value = currentTicks % (max - min) + min;
+
+            return (int)value;
         }
     }
 }
